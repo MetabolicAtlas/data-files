8c3d74eb1123c0ef93a3527b7e2f3b8480bef3c3
nanjiangshu <nanjiang.shu@gmail.com>
2022-04-28 14:41:54 +0200

    feat: update the data to match the data format

--- utils/fetch_release_data.py
+++ utils/fetch_release_data.py
@@ -8,6 +8,18 @@ import json
 import argparse
 from github import Github
 
+# define data for externalParentId
+EXT_PARENT_ID = {
+        }
+
+# define PMID for each specific release (if there is any)
+RELEASE_PMID_DICT = {
+        }
+
+# define PMID for each GEM
+GEM_PMID_DICT = {
+        }
+
 
 def writefile(content, outfile, mode='w', is_flush=False):
     """Write text to file"""
@@ -20,24 +32,51 @@ def writefile(content, outfile, mode='w', is_flush=False):
     except IOError:
         print(f"Failed to write to {outfile}", file=sys.stderr)
 
+def get_count(repo_name, api_token):
+    """Get count for genes, reactions and metabolites"""
+    # To be implemented
+    return {}
+
+
+def get_validation(repo_name, api_token):
+    """Get quality score for the model"""
+    # To be implemented
+    return {}
+
 
 def get_release_data(repo_name, git_api):
     """Get release data from Github"""
     repo = git_api.get_repo(repo_name)
     releases = repo.get_releases()
     gem_name = os.path.basename(repo_name)
-    gem_data = {}
+    gem_data_list = []
     for rel in releases:
+        if rel.prerelease:  # ignore pre releases
+            continue
         version = rel.tag_name.lstrip('v')
-        gem_data[version] = {}
-        gem_data[version]['title'] = rel.title
-        gem_data[version]['id'] = f"{gem_name}-{version}"
-        gem_data[version]['date'] = rel.published_at.strftime("%Y-%m-%d")
-        gem_data[version]['tag_name'] = rel.tag_name
-        gem_data[version]['link'] = rel.html_url
-        gem_data[version]['version'] = rel.tag_name.lstrip('v')
-        gem_data[version]['prerelease'] = rel.prerelease
-    return gem_data
+        li = version.split('-')
+        if len(li) > 1:  # ignore alpha or beta releases
+            continue
+        gem_data = {}
+        rel_id = f"{gem_name}-{version}"
+        gem_data['id'] = rel_id
+        if rel_id in EXT_PARENT_ID:
+            gem_data['externalParentId'] = EXT_PARENT_ID[rel_id]
+        else:
+            gem_data['externalParentId'] = []
+        gem_data['releaseDate'] = rel.published_at.strftime("%Y-%m-%d")
+        gem_data['releaseLink'] = rel.html_url
+        if rel_id in RELEASE_PMID_DICT:
+            gem_data['PMID'] = RELEASE_PMID_DICT[rel_id]
+        elif gem_name in GEM_PMID_DICT:
+            gem_data['PMID'] = GEM_PMID_DICT[gem_name]
+        else:
+            gem_data['PMID'] = ''
+        gem_data['count'] = get_count(repo_name, git_api)
+        gem_data['validation'] = get_validation(repo_name, git_api)
+        gem_data_list.append(gem_data)
+
+    return gem_data_list
 
 
 def get_integrated_gems_list(repo_name, git_api):
@@ -55,15 +94,13 @@ def generate_data(git_api, basedir, is_dryrun):
     """Fetch data from Github and output the result in JSON format"""
     owner = "SysBioChalmers"
     gems_list = get_integrated_gems_list("MetabolicAtlas/data-files", git_api)
-    all_data = {}
     for gem_name in gems_list:
         repo_name = f"{owner}/{gem_name}"
         gem_release_data = get_release_data(repo_name, git_api)
-        all_data[gem_name] = gem_release_data
         outfile = os.path.join(basedir, 'integrated-models', gem_name,
                                'gemRepository.json')
         if not is_dryrun:
-            writefile(json.dumps(all_data, indent=2), outfile)
+            writefile(json.dumps(gem_release_data, indent=2), outfile)
             msg = f"Result file output to {outfile}"
         else:
             msg = f"DryRun: result file will be output to {outfile}"
